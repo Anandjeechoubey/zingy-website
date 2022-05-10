4cf9d252423b96dbb4064b6e89ed14c2beefb6f6
Anand Jee Choubey <anandjeechoubey@Anands-MacBook-Air.local>
2022-05-10 21:31:22 +0530

    [fix] Mob View
    Signed-off-by: Anand Jee Choubey <anandjeechoubey@Anands-MacBook-Air.local>

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 const IndexPage = () => (
   <Layout title="Home">
     <div className="w-full h-full flex items-center justify-center absolute">
-      <div className="max-w-lg bg-white p-36 rounded-3xl shadow-2xl">
+      <div className="max-w-lg bg-white p-16 md:p-36 rounded-3xl shadow-2xl">
         <img
           src="https://zingy-public-media.s3.ap-south-1.amazonaws.com/Zingy.png"
           alt="Zingy Music"
