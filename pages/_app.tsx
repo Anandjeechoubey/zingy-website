4b92545eeee4162ec8a1b1ed158fd4b25ff921aa
Anand Jee Choubey <anandjeechoubey@Anands-MacBook-Air.local>
2022-05-28 15:47:32 +0530

    add: G-Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { AppProps } from "next/app";
+import Script from "next/script";
 
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-02CR5P3Z91`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-02CR5P3Z91', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
